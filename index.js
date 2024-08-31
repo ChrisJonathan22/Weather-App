9159b78c42c70550f6889380da43d8a3269083d6
Chris Jonathan <chris@chrisjonathan.co.uk>
2024-08-31 21:30:45 +0100

    Added error handling and the ability to retrieve data for the selected city

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 import apiKey from "./apiKey.js";
 
 let cityWeatherInfo = {};
+let cityDropdown = document.querySelector("#cityDropdown");
 
 const cityEl = document.querySelector(".cityName");
 const cityWeatherDescEl = document.querySelector(".cityWeatherDescription");
 const cityTemperatureEl = document.querySelector(".cityWeatherTemperature");
 
 // Function to get city latitude and longitude
-async function getCityLatAndLon (city) {
+async function getCityLatAndLon (city="London") {
     const latAndLonApi = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
-    const response = await fetch(latAndLonApi);
-    const weatherData = await response.json();
-    const cityLat = weatherData[0].lat;
-    const cityLon = weatherData[0].lon;
+    try {
+        const response = await fetch(latAndLonApi);
+        const weatherData = await response.json();
+        const cityLat = weatherData[0].lat;
+        const cityLon = weatherData[0].lon;
 
-    getCityWeatherData(cityLat, cityLon, city);
+        getCityWeatherData(cityLat, cityLon, city);
+    }
+    catch (error) {
+        console.log("There was an error.", error.message);
+    }
 }
 
 // Function to get city weather data
 async function getCityWeatherData (lat, lon, city) {
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
-    const response = await fetch(weatherApi);
-    const weatherData = await response.json();
 
-
-    // Assing the data to the city object
-    cityWeatherInfo.city = city;
-    cityWeatherInfo.description = weatherData.weather[0].description;
-    cityWeatherInfo.icon = weatherData.weather[0].icon;
-    cityWeatherInfo.temp = weatherData.main.temp;
-
-    console.log("Final data: ", cityWeatherInfo);
-
+    try {
+        const response = await fetch(weatherApi);
+        const weatherData = await response.json();
     
-    // Display the data on the frontend
-    cityEl.innerHTML = cityWeatherInfo.city;
-    cityWeatherDescEl.innerHTML = cityWeatherInfo.description;
-    cityTemperatureEl.innerHTML = cityWeatherInfo.temp;
+        // Assing the data to the city object
+        cityWeatherInfo.city = city;
+        cityWeatherInfo.description = weatherData.weather[0].description;
+        cityWeatherInfo.icon = weatherData.weather[0].icon;
+        cityWeatherInfo.temp = weatherData.main.temp;
+    
+        
+        // Display the data on the frontend
+        cityEl.innerHTML = cityWeatherInfo.city;
+        cityWeatherDescEl.innerHTML = cityWeatherInfo.description;
+        cityTemperatureEl.innerHTML = cityWeatherInfo.temp;
+    }
+    catch (error) {
+        console.log("There was an error.", error.message);
+    }
 }
 
+getCityLatAndLon("London");
 
-getCityLatAndLon("Paris");
+cityDropdown.addEventListener("change", (e) => {
+    let selectedCity = e.target.value;
+    getCityLatAndLon(selectedCity);
+});
